723c0d40780ebbddc5abbeb7f2c4944c52fe1d26
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-10-12 16:45:43 -0600

    Recepcion de id a editar
    Recibimos el id del empleado a editar de la url y se imprime en consola

--- src/app/componentes/editar-empleado/editar-empleado.component.ts
+++ src/app/componentes/editar-empleado/editar-empleado.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+//para obtener el id de la ruta
+import { Router, ActivatedRoute } from '@angular/router';
+//recibir datos del formulario
+import { FormGroup, FormBuilder } from '@angular/forms';
+//llamar al servicio creado
+import { CrudService } from 'src/app/servicio/crud.service';
 
 @Component({
   selector: 'app-editar-empleado',
   templateUrl: './editar-empleado.component.html',
-  styleUrls: ['./editar-empleado.component.css']
+  styleUrls: ['./editar-empleado.component.css'],
 })
 export class EditarEmpleadoComponent {
+  //recibimos el id del usuario a editar
+  elID: any;
 
+  constructor(private activeRoute: ActivatedRoute) {
+    //recuperar el id , de la liga capturas el parametro id
+    this.elID = this.activeRoute.snapshot.paramMap.get('id');
+    console.log(this.elID);
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 }
