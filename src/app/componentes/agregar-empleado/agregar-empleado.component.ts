43d8c1c8e783716112b10021120b3ac65188d9da
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-10-12 12:13:10 -0600

    Ejecutar método en el form
    Se envian datos correctamente desde el formulario y se imprimen en pantalla

--- src/app/componentes/agregar-empleado/agregar-empleado.component.ts
+++ src/app/componentes/agregar-empleado/agregar-empleado.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
+//recibir datos del formulario
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-empleado',
   templateUrl: './agregar-empleado.component.html',
-  styleUrls: ['./agregar-empleado.component.css']
+  styleUrls: ['./agregar-empleado.component.css'],
 })
 /**
  * Recepciona los datos o los maneja del propio html 'agregar-empleado'
  */
 export class AgregarEmpleadoComponent {
+  //recuperar el formulario (tienes todos los datos que llegan directamente del formulario)
+  formularioEmpleados: FormGroup;
+
+  //usar el builder en el constructor (para recibir los datos)
+  constructor(public formulario: FormBuilder) {
+    //usar los datos del formulario para almacenarlos/procesarlos los recibe en froma de grupo
+    this.formularioEmpleados = this.formulario.group({
+      //deben coincidir con los nombres de los controles
+      nombre: [''],
+      correo: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 
   //devuelve cualquier tipo de dato que llegue del formulario
-  enviarDatos():any{
-    console.log("Hiciste clic en agregar");
+  enviarDatos(): any {
+    console.log('Hiciste clic en agregar');
+    //imprime los datos recibidos del formulario
+    console.log(this.formularioEmpleados.value);
   }
 }
