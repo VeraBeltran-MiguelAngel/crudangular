b89e2dfc20c99f6fbe2a0994ca13d7cdfa2233e2
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-10-12 11:34:30 -0600

    Ejecutar metodo en form
    Se crea el metodo enviarDatos en el c.ts de agregar empleado aqui se manejan los datos que vengan del html

--- src/app/componentes/agregar-empleado/agregar-empleado.component.ts
+++ src/app/componentes/agregar-empleado/agregar-empleado.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './agregar-empleado.component.html',
   styleUrls: ['./agregar-empleado.component.css']
 })
+/**
+ * Recepciona los datos o los maneja del propio html 'agregar-empleado'
+ */
 export class AgregarEmpleadoComponent {
 
+  //devuelve cualquier tipo de dato que llegue del formulario
+  enviarDatos():any{
+    console.log("Hiciste clic en agregar");
+  }
 }
