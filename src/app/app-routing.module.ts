45fcd9af1f4fb3b95b35fe39b4fa66d12e980db1
Miguel Angel Vera Beltrán <miguel-angel.vera-beltran@outlook.com>
2023-10-12 00:00:15 -0600

    Agregamos rutas de nuestros componentes al menu nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //cuando el usuario escriba una direccion vacia lo envia a agregar-empleado
+  { path: '', pathMatch: 'full', redirectTo: 'agregar-empleado' },
+  //si escribe agregar-empleado lo dirige al componente
+  { path: 'agregar-empleado', component: AgregarEmpleadoComponent },
+  { path: 'listar-empleado', component: ListarEmpleadoComponent },
+  //enviamos un parametro id a la ruta
+  { path: 'editar-empleado/:id', component: EditarEmpleadoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
