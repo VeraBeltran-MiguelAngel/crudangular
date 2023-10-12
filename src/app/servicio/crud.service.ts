0bf4f2262a45440d38a7d5633c1de52d51b4131e
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-10-12 12:43:18 -0600

    Preparando API en servicio
    Configuramos metodo agregar en el service pra que vaya al metodo insertar de la API

--- src/app/servicio/crud.service.ts
+++ src/app/servicio/crud.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+//para conectarse al api
+import { HttpClient } from '@angular/common/http';
+//monitorear que esta sucediendo con la informacion externa
+import { Observable } from 'rxjs';
+//modelo empleados
+import { Empleado } from './Empleado';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CrudService {
+  //variable que guarda el endpoint url
+  API: string = 'http://localhost/empleados';
+  constructor(private clienteHttp: HttpClient) {}
 
-  constructor() { }
+  AgregarEmpleado(datosEmpleado: Empleado): Observable<any> {
+    //para insertar datos en el api
+    return this.clienteHttp.post(this.API + '?insertar=1', datosEmpleado);
+  }
 }
