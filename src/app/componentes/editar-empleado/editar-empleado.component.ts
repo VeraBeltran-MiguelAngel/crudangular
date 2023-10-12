df8ea80940a3adc4b05306a7a17a90d7f244c180
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-10-12 17:15:14 -0600

    Recuperando datos a  editar
    Ya se observan los datos del empleado en el formulario editar

--- src/app/componentes/editar-empleado/editar-empleado.component.ts
+++ src/app/componentes/editar-empleado/editar-empleado.component.ts
@@ -12,21 +12,38 @@ import { CrudService } from 'src/app/servicio/crud.service';
   styleUrls: ['./editar-empleado.component.css'],
 })
 export class EditarEmpleadoComponent {
+  //recuperar el formulario (tienes todos los datos que llegan directamente del formulario)
+  formularioEmpleados: FormGroup;
   //recibimos el id del usuario a editar
   elID: any;
 
   constructor(
     private activeRoute: ActivatedRoute,
-    private crudService: CrudService
+    private crudService: CrudService,
+    public formulario: FormBuilder
   ) {
     //recuperar el id , de la liga capturas el parametro id
     this.elID = this.activeRoute.snapshot.paramMap.get('id');
     console.log(this.elID);
-    this.crudService.ObtenerEmpleado(this.elID).subscribe(respuesta =>{
+    this.crudService.ObtenerEmpleado(this.elID).subscribe((respuesta) => {
       //mostramos los datos que vienen de la solicitud
       console.log(respuesta);
+      //asignar los valores que vienen de la respuesta en el formulario editar
+      this.formularioEmpleados.setValue({
+        //estos dos datos se asignan al formulario para que tengan esos valores 
+        //valor 0 que viene de la API
+        nombre:respuesta[0]['nombre'], 
+        //valor 0 que viene de la API
+        correo:respuesta[0]['correo']
+      });
+    });
+    this.formularioEmpleados = this.formulario.group({
+      //deben coincidir con los nombres de los controles
+      nombre: [''],
+      correo: [''],
     });
   }
+
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
